0cd1e94fba82487718669fb70977525b2f0667be
sultanmyrza <sultanmyrza@sultanmyrzas-iMac.local>
2018-06-21 14:35:30 +0900

    I dont know

--- handler.js
+++ handler.js
@@ -15,6 +15,7 @@ function responseSuccess(body) {
 function responseError(error) {
   return {
     statusCode: error.statusCode || 500,
+    headers: { 'Content-Type': 'plain/text' },
     body: JSON.stringify(error.message)
   };
 }
@@ -86,7 +87,6 @@ module.exports.getOne = (event, context, callback) => {
       .catch(err =>
         callback(null, {
           statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'plain/text' },
           body: JSON.stringify(err)
         })
       );
@@ -100,18 +100,25 @@ module.exports.getAll = (event, context, callback) => {
     Kortoon.find()
       .then(kortoons => {
         console.log(kortoons);
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify(kortoons)
-        });
+        callback(null, responseSuccess(kortoons));
       })
-      .catch(err =>
-        callback(null, {
-          statusCode: err.statusCode || 500,
-          headers: { 'Content-Type': 'plain/text' },
-          body: JSON.stringify(err.message)
-        })
-      );
+      .catch(err => callback(null, responseError(err.message)));
+  });
+};
+
+module.exports.deleteAll = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase().then(() => {
+    Kortoon.remove({}, function(err, result) {
+      if (err) {
+        console.log(result);
+        callback(null, responseError(err.message));
+      } else {
+        console.log(result);
+        callback(null, responseSuccess(result));
+      }
+    });
   });
 };
 
--- models/Kortoon.js
+++ models/Kortoon.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
+
+const SceneSchema = new mongoose.Schema({
+  alt: String,
+  src: String,
+  srcFromCloudinary: String
+});
+
+const EpisodeSchema = new mongoose.Schema({
+  title: String,
+  url: String,
+  episodeIndex: Number,
+  scenes: [SceneSchema]
+});
+
 const KortoonSchema = new mongoose.Schema({
   title: String,
-  description: String
+  summary: String,
+  thumbnailUrl: String,
+  thumbnailUrlCloudinary: String,
+  PhotoUrl: String,
+  PhotoUrlCloudinary: String,
+  url: String,
+  episodes: [EpisodeSchema]
 });
+
 module.exports = mongoose.model('Kortoon', KortoonSchema);
